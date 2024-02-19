28d0989748722cd8fcfab99e07b65d37a4a6f0e0
Md. Abdul Kader <abdulemail789@gmail.com>
2024-02-19 19:32:59 +0600

    created product card fetching fakestore api

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+const container = document.querySelector('.container');
+
+// fetch product data
+fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => displayProduct(data));
+
+
+
+function displayProduct(products) {
+    products.forEach((product) => {
+
+        // Creating elements
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product-card');
+        productDiv.innerHTML = `
+      <div class="product-tumb">
+        <img src="${product.image}" alt="${product.title}">
+      </div>
+      <div class="product-details">
+        <h4><a href="">${product.title.slice(0, 30)}</a></h4>
+        <p>${product.description.slice(0, 120)}</p>
+        <div class="product-bottom-details">
+          <div class="product-price">$ ${product.price}</div>
+          <button class='add-to-cart'>Add To Cart</button>
+        </div>
+      </div>`;
+
+        // Appending elements
+        container.appendChild(productDiv);
+    });
+}
+
+
+
+
+
+
+
+
+
+
